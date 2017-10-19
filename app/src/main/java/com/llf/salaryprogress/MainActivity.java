d68c62bb0662ce83b3f64a4d2dd3cc7a0518a7d0
刘小帅 <854034241@qq.com>
2017-10-19 09:52:04 +0800

    Update MainActivity.java

--- app/src/main/java/com/llf/salaryprogress/MainActivity.java
+++ app/src/main/java/com/llf/salaryprogress/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //直接在github上修改代码
         setContentView(R.layout.activity_main);
     }
 }
